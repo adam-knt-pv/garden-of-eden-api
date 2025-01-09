20300a8f34219c5ea66515b3a180b34a0f232b86
pathmage <pathmage@outlook.com>
2025-01-09 19:22:33 +0100

    PropertySetters extensions

---
+++ utilities/extensions/PropertySetters.cs
@@ -0,0 +1,76 @@
+﻿namespace pathmage.KnightmareEngine;
+
+partial class Extensions
+{
+	public static Vector2 X(this Vector2 vector, float x)
+	{
+		vector.X = x;
+		return vector;
+	}
+
+	public static Vector2 Y(this Vector2 vector, float y)
+	{
+		vector.Y = y;
+		return vector;
+	}
+
+	public static Vector2I X(this Vector2I vector, int x)
+	{
+		vector.X = x;
+		return vector;
+	}
+
+	public static Vector2I Y(this Vector2I vector, int y)
+	{
+		vector.Y = y;
+		return vector;
+	}
+
+	public static Vector3 X(this Vector3 vector, float x)
+	{
+		vector.X = x;
+		return vector;
+	}
+
+	public static Vector3 Y(this Vector3 vector, float y)
+	{
+		vector.Y = y;
+		return vector;
+	}
+
+	public static Vector3I X(this Vector3I vector, int x)
+	{
+		vector.X = x;
+		return vector;
+	}
+
+	public static Vector3I Y(this Vector3I vector, int y)
+	{
+		vector.Y = y;
+		return vector;
+	}
+
+	public static Vector4 X(this Vector4 vector, float x)
+	{
+		vector.X = x;
+		return vector;
+	}
+
+	public static Vector4 Y(this Vector4 vector, float y)
+	{
+		vector.Y = y;
+		return vector;
+	}
+
+	public static Vector4I X(this Vector4I vector, int x)
+	{
+		vector.X = x;
+		return vector;
+	}
+
+	public static Vector4I Y(this Vector4I vector, int y)
+	{
+		vector.Y = y;
+		return vector;
+	}
+}
